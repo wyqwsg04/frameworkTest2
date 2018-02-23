5767c74ef874bf342e6f7c681cb2889d30aa9a45
l's'c <fdzclsc@163.com>
2018-02-23 19:22:44 +0800

    Add files via upload

---
+++ frameworkTest2.framework/Headers/LinTest.h
@@ -0,0 +1,19 @@
+//
+//  LinTest.h
+//  LinTest
+//
+//  Created by linshucan on 2018/2/23.
+//  Copyright © 2018年 Bee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinTest.
+FOUNDATION_EXPORT double LinTestVersionNumber;
+
+//! Project version string for LinTest.
+FOUNDATION_EXPORT const unsigned char LinTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinTest/PublicHeader.h>
+
+
---
+++ frameworkTest2.framework/Headers/LinWebView.h
@@ -0,0 +1,13 @@
+//
+//  LinWebView.h
+//  LinTest
+//
+//  Created by linshucan on 2018/2/23.
+//  Copyright © 2018年 Bee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinWebView : UIWebView
+
+@end
